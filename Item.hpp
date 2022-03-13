a4390cd5ea66b67d14a318879ec0069f5b077126
Adzka <voguelish7@gmail.com>
2022-03-13 18:47:08 +0700

    Header Class Item

---
+++ Item.hpp
@@ -0,0 +1,48 @@
+#ifndef ITEM_HPP
+#define ITEM_HPP
+
+#define MAX_QTY 64
+#define MAX_DURABILITY 10
+
+#include <string>
+using namespace std;
+
+class Item {
+    protected:
+        int id;
+        string name;
+        string type;
+        int quantity;
+
+    public:
+        Item(); 
+        Item(int id, string name, string type, int quantity);
+        int get_id();
+        int get_name();
+        int get_type();
+        int get_quantity();
+        void set_quantity(int quantity);
+        void add_quantity(int quantity);
+        void remove_quantity(int quantity);
+
+};
+class Tool : public Item {
+    public:
+        Tool();
+        Tool(int id, string name, string type, int quantity);
+
+};
+class NonTool : public Item {
+    private:
+        int durability;
+    public:
+        NonTool();
+        NonTool(int id, string name, string type, int durability);
+        int get_durability();
+        void set_durability(int durability);
+        void add_durability(int durability);
+        void remove_durability(int durability);
+        
+};
+
+#endif
